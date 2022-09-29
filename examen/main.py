efd10f74ca827b8e370d747ad77e4151438b2d92
Azul Briones <you@example.com>
2022-09-29 18:24:00 -0500

    Update threads with random time

--- examen/main.py
+++ examen/main.py
@@ -1,4 +1,6 @@
 import threading
+import time
+import random
 
 objects = []
 peopleEat = []
@@ -19,25 +21,29 @@ def catch(id):
         return False
     
 # Se liberan los palillos cercanos a la persona
-def free(id):
+def free(id, timeEat):
     objects[id].release()
     objects[(id-1) % peopleN].release()
-    print(f"Persona {id} terminó de comer.")
+    print(f"Persona {id+1} terminó de comer en {timeEat} segundos.\n")
 
 # Proceso de la persona comiendo y las personas esperando  
 def eating(id):
     
     if catch(id):
-        print(f"Persona {id} comiendo")
-        peopleEat.append(id)
+        print(f"Persona {id+1} comiendo")
+        peopleEat.append(id+1)
         
         difference_1 = set(peopleTotal).difference(set(peopleEat))
         difference_2 = set(peopleEat).difference(set(peopleTotal))
         list_difference = list(difference_1.union(difference_2))
-    
+
         print(f"Personas esperando: {list_difference}")
         
-        free(id)
+        # Tiempo que tarda cada hilo en ejecutarse, entre 0 y 5 segundos
+        timeEat = random.uniform(0,5)
+        time.sleep(timeEat)
+        
+        free(id, timeEat)
             
 if __name__ == '__main__':
     hilos = []
@@ -45,12 +51,13 @@ if __name__ == '__main__':
     # Inicialización de palillos
     for _ in range(peopleN):
         objects.append(threading.Lock())
-        
+    
+    # Inicialización de personas e hilos    
     for i in range(peopleN):
-        peopleTotal.append(i)
+        peopleTotal.append(i+1)
         nuevo_hilo = threading.Thread(target=eating, args=(i,))
         hilos.append(nuevo_hilo)
     
-    # Iniciar ejecución de los hilos
+    # Ejecución de los hilos
     for hilo in hilos:
         hilo.start()
