5183386128d811ff8d22630328ce3d966b3fbc86
Azul Briones <you@example.com>
2022-11-02 19:33:49 -0600

    Tribu problem done

--- monitores/tribu.py
+++ monitores/tribu.py
@@ -1,73 +1,91 @@
 import threading
 import time
+import random
+from queue import Queue
 
-MIEMBROS = 12
-MAX_OLLA = 12
-COCINEROS = 1
+MIEMBROS = 8
+MAX_OLLA = 10
 
-olla = []
-mutex = threading.Lock()
-miembro = threading.Condition(mutex)
-cocinero = threading.Condition(mutex)
-
-class Miembros(threading.Thread):
-  conta = 1
-  def __init__(self):
-    super(Miembros, self).__init__()
-    self.id  = Miembros.conta
-    Miembros.conta += 1
-
-  def comer(self):
-    with mutex:
-        while not olla:
-            print(f"Olla vacía {olla}. Se despierta al cocinero...\nMiembro {self.id} esperando...\n")
-            miembro.wait()
-            cocinero.notify()
-        olla.pop()
-        print(f"Miembro {self.id} se sirve de la olla. Misioneros en la olla {olla}\n")
-        time.sleep(2)
-                 
-  def run(self):
-    for _ in range(MIEMBROS):
-      self.comer()
-      
-class Cocineros(threading.Thread):
+class Tribu():
+  mutex = threading.Lock()
+  miembros = threading.Condition()
+  cocinero = threading.Condition()
+  
+  olla = Queue(MAX_OLLA)
   
   def __init__(self):
-    super(Cocineros, self).__init__()
-    self.id  = 1
+    super(Tribu, self).__init__()
+    
+  def comer(self, miembro):
+    self.miembros.acquire()
+    self.mutex.acquire()
+    if self.olla.empty():
+      print(f"Olla vacía: {self.olla.qsize()}. Se despierta al cocinero...\nMiembro {miembro.id} esperando...\n")
+      self.miembros.wait()
+      self.miembros.release()
+      self.cocinero.acquire()
+      self.cocinero.notify()
+      self.cocinero.release()
+    else:
+      time.sleep(1)
+      self.olla.get()
+      self.mutex.release()
+      print(f"Miembro {miembro.id} se sirve de la olla. Misioneros en la olla {self.olla.qsize()}\n")
+      self.miembros.release()
     
   def cocinar(self):
-    if len(olla) == 0:
-      while len(olla) < MAX_OLLA:
-        print(f"Cocinero {self.id} preparando misioneros xd...\nOlla {olla}\n")
-        item = 0
-        olla.append(item)
-        time.sleep(2)
-    if len(olla) == MAX_OLLA:
-      print(f"Olla llena. El cocinero {self.id} va a descansar...\nOlla {olla}\n")
-      cocinero.wait()
-      miembro.notify_all()
+    while True:
+      time.sleep(1)
+      self.cocinero.acquire()
       
-  def run(self):
-    for _ in range(COCINEROS):
-      self.cocinar()
-
-def main():    
-  miembros = []
-  cocineros = []
-
-  for i in range(MIEMBROS):
-    miembros.append(Miembros())
-      
-  for i in range(COCINEROS):
-    cocineros.append(Cocineros())
-
-  for m in miembros:
-    m.start()
+      if self.olla.full():
+        print(f"Cocinero descansando. Olla llena: {self.olla.qsize()}\n")
+        self.mutex.release()
+        self.miembros.acquire()
+        self.miembros.notify()
+        self.miembros.release() 
+        self.cocinero.release() 
+        
+      if self.olla.empty():
+        while self.olla.qsize()<MAX_OLLA:
+          time.sleep(1)
+          misionero = random.randint(0, 100)
+          self.olla.put(misionero)
+          print(f"Cocinero preparando misionero. Misioneros en la olla {self.olla.qsize()}\n") 
     
-  for c in cocineros:
-    c.start()
+class Miembros(threading.Thread):
+  conta = 1
+  
+  def __init__(self):
+    super(Miembros, self).__init__()
+    self.id = Miembros.conta
+    Miembros.conta += 1
+  
+  def run(self):
+    for _ in range(MIEMBROS):
+      time.sleep(1)
+      tribu.comer(self)
 
-if __name__ == "__main__":
-    main()
+class Cocinero(threading.Thread):
+  def __init__(self):
+    super(Cocinero, self).__init__()
+    self.id = 1
+    
+  def run(self):
+    time.sleep(1)
+    tribu.cocinar()
+    
+def main():
+  threads = []
+  
+  for i in range(MIEMBROS): 
+    threads.append(Miembros())
+    
+  threads.append(Cocinero())
+  
+  for t in threads:
+    t.start()
+    
+if __name__ == '__main__':
+  tribu = Tribu()
+  main()
